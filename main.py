8fb1ccd7d69a90a77140051fd00bbec3edb52ebf
NewPalace <cristian204@outlook.es>
2020-08-02 04:06:44 -0500

    Actualizacion 3

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
 import pygame, random
 from clases import Player, General1, Lancero1, Lancero2, vida
-from clases import  Fondo, Muro1, Barra1, imagenblock, Escudo
+from clases import  Fondo, Muro1, Barra1, imagenblock, Escudo, Game_Over
 from variable import white,skyblue, green, black,green, sc_ancho, sc_largo, ticK, Textbarup, bax, bay, Nfle, Lanlvl1, explanlv1
 from variable import mflefir, mflenor
 from Imagenes import  Icono
 from Controles import Fnormal, Ffire
 
+pygame.init()
 pygame.mixer.init()
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode([sc_largo, sc_ancho])
     clock = pygame.time.Clock()
-    score = 0
-    Flechas = Nfle
-
-    #Personalizacion de la ventana
-    pygame.display.set_caption(Textbarup)
-    icon = pygame.image.load(Icono)
-    icon.set_colorkey(black)
-    pygame.display.set_icon(icon)
-
-    #Guardado de sprites
-    all_sprites = pygame.sprite.Group()
-    lancerolvl1 = pygame.sprite.Group()
-    lancerolvl2 = pygame.sprite.Group()
-    lancerolvl22 = pygame.sprite.Group()
-    bullets = pygame.sprite.Group()
-    bullets2 = pygame.sprite.Group()
 
     #"inicializacion" de clases
     fondo = Fondo()
@@ -41,36 +26,61 @@ def main():
     lazul = Lancero1()
     eazul = Lancero2()
     NumeroDeFlechas = vida(bax+38,bay+25)
+    Gameo = Game_Over(screen)
 
-    all_sprites.add(escudo)
-
-    #generacion de enemigos (se pasara a una clase cuando se termine)
-    for i in range(Lanlvl1):
-        lazul = Lancero1()
-        lancerolvl1.add(lazul)
-        all_sprites.add(lazul)
-    #for i in range(1):
-     #   eazul = Lancero2()
-      #  lancerolvl2.add(eazul)
-       # all_sprites.add(eazul)
-    #eazul1 = Lancero2()
-    #eazul2 = Lancero2()
-    #eazul3 = Lancero2()
-    #eazul4 = Lancero2()
-
-    #lancerolvl2.add(eazul1)
-    #lancerolvl2.add(eazul2)
-    #lancerolvl2.add(eazul3)
-    #lancerolvl2.add(eazul4)
-
-    #all_sprites.add(eazul1)
-    #all_sprites.add(eazul2)
-    #all_sprites.add(eazul3)
-    #all_sprites.add(eazul4)
-
+    #Personalizacion de la ventana
+    pygame.display.set_caption(Textbarup)
+    icon = pygame.image.load(Icono)
+    icon.set_colorkey(black)
+    pygame.display.set_icon(icon)
+  
+    game_over = True
     done = False
 
     while not done:
+        if game_over:
+
+            #Gameo.show_go_screen()
+            Texto_1 = vida(bax+80,sc_ancho//3)
+            Texto_2 = vida(bax+225,sc_ancho//2)
+            Texto_3 = vida(bax+250,sc_ancho*2//3)
+            Texto_1.draw_text(screen, "OE PERDISTE? xd", 65)
+            Texto_2.draw_text(screen, "Na mentira, xd", 25)
+            Texto_3.draw_text(screen, "Press SPACE", 20)
+            pygame.display.flip()
+            waiting = True
+            while waiting:
+                clock.tick(60)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_SPACE:
+                            waiting = False
+#----------------------------------------------------------------------------
+            game_over = False
+            #Guardado de sprites
+            all_sprites = pygame.sprite.Group()
+            lancerolvl1 = pygame.sprite.Group()
+            lancerolvl2 = pygame.sprite.Group()
+            lancerolvl22 = pygame.sprite.Group()
+            bullets = pygame.sprite.Group()
+            bullets2 = pygame.sprite.Group()
+
+            all_sprites.add(escudo)
+            score = 0
+            Flechas = Nfle
+ 
+            #generacion de enemigos (se pasara a una clase cuando se termine)
+            #Lanceros
+            for i in range(Lanlvl1):
+                lazul = Lancero1()
+                lancerolvl1.add(lazul)
+                all_sprites.add(lazul)
+            
+            #Escuderos
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -78,9 +88,13 @@ def main():
                 if event.key == Fnormal:
                     Play.Nshoot(bullets, all_sprites)
                     Flechas -= mflenor
+                    if Flechas <= 0:
+                        game_over = True
                 if event.key == Ffire:
                     Play.Fshoot(bullets2,all_sprites)
                     Flechas -= mflefir
+                    if Flechas <= 0:
+                        game_over = True
         #Imagen de fondo  
         screen.blit(fondo.image, fondo.rect)
         all_sprites.draw(screen)
@@ -125,13 +139,13 @@ def main():
         for hit in hits:
             escudo.shield = escudo.shield - 3
             if escudo.shield <= 0:
-                done =True
+                game_over =True
 
         hits = pygame.sprite.spritecollide(escudo, lancerolvl2, False)
         for hit in hits:
             escudo.shield = escudo.shield - 5
             if escudo.shield <= 0:
-                done =True
+                game_over =True
             
         all_sprites.update()
             
